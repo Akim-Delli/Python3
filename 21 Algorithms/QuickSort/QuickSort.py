757cc4a785792838f0a0a6cc0950c16c096606dd
Akim Delli <adelli@liveintent.com>
2015-07-24 00:11:18 -0400

    Algorithm: quickSort

---
+++ 21 Algorithms/QuickSort/QuickSort.py
@@ -0,0 +1,14 @@
+__author__ = 'adelli'
+
+def quickSort(input = []):
+    '''Quicksort Algorithm'''
+    if input == []: return [] # Base case
+
+    pivot = [element for element in input if element == input[0]]
+    leftPartition = [element for element in input if element < pivot[0]]
+    rightPartition = [element for element in input if element > pivot[0]]
+    return quickSort(leftPartition) + pivot + quickSort(rightPartition)
+
+
+sortedList = quickSort([2, 5,1, 7, 9, 3, 19, 7, 3, 8])
+print(sortedList)
