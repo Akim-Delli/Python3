becd27ef42d570cfd61ef9f0e1488cc848eed7d6
Akim Delli <adelli@liveintent.com>
2015-07-29 22:46:35 -0400

    regular expression 2

---
+++ 09 Regexes/regex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# regex.py by Akim Delli
+
+
+
+import re
+
+def main():
+    fh = open('raven.txt')
+    for line in fh:
+        match = re.search('(Len|Neverm)ore', line)
+        if re.search('(Len|Neverm)ore', line):
+            if match:
+                print(match.group())
+
+if __name__ == "__main__": main()
